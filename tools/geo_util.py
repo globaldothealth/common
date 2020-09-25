25a3bbcf8197db8448260b47168389b4220b2c29
Manu Cornet <m@ma.nu>
2020-09-25 12:29:49 +0000

    Add 'geo_utils'

---
+++ tools/geo_util.py
@@ -0,0 +1,15 @@
+LAT_LNG_DECIMAL_PLACES = 4
+
+def round_latlng(l):
+    if "." not in l:
+        l += ".0000"
+    (intpart, dec) = l.split(".")
+    if len(dec) > LAT_LNG_DECIMAL_PLACES:
+        # Just truncate
+        dec = dec[0:4]
+    elif len(dec) < LAT_LNG_DECIMAL_PLACES:
+        dec += "0" * (LAT_LNG_DECIMAL_PLACES - len(dec))
+    return intpart + "." + dec
+
+def make_geoid(lat, lng):
+    return round_latlng(lat) + "|" + round_latlng(lng)
